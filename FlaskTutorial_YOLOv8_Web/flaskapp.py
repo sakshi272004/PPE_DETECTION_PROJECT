87122d487d28330914ae2b2bdb034a84b1642ebc
sakshi chavhan <sakshi.chavhan@mitaoe.ac.in>
2024-09-11 22:29:20 -0700

    FIrst Commit

--- FlaskTutorial_YOLOv8_Web/flaskapp.py
+++ FlaskTutorial_YOLOv8_Web/flaskapp.py
@@ -92,8 +92,10 @@ def video():
 # To display the Output Video on Webcam page
 @app.route('/webapp')
 def webapp():
-    #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
     
+    #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames_web(path_x=session.get('video_path', None)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
